bc539fc081158847f487d58b97f79a51af6c270a
Your Full Name <ehasun0909@gamil.com>
2025-07-17 16:11:35 +0600

    create static folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-6">
       <h3 className="text-3xl font-extrabold">This is next js class (3)</h3>
 
       <p>
---
+++ src/app/static/page.js
@@ -0,0 +1,12 @@
+export default function Static() {
+  return (
+    <div className="flex flex-col gap-6">
+      <h3 className="text-3xl font-extrabold">Static page</h3>
+
+      <p>
+        Work on Static
+        এই Static পেজ এ যখন -npm run build- করি তার পরে যদি কোণ change করি তখন কোণ কিছু page a change hoi na tar mane holo at akta static page . 
+      </p>
+    </div>
+  );
+}
